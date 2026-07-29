8350b60bc9ef925063168ced66fa1ac0c1379007
teekayyj-dotcom <letuankiet0012@gmail.com>
2026-07-29 13:35:21 +0700

    Fix frontend build import path

--- frontend/src/modules/client-view/pages/PublicReviewPage.tsx
+++ frontend/src/modules/client-view/pages/PublicReviewPage.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import { fetchApi } from "../../../utils/apiClient";
+import { fetchApi } from "../utils/apiClient";
 import { ClientPlaybackPage } from "./ClientPlaybackPage";
 import { Loader2, ArrowRight } from "lucide-react";
 
