82e437b640b209c72d2efa9f8a2c8cc040c4aaca
teekayyj-dotcom <letuankiet0012@gmail.com>
2026-09-10 11:12:38 +0700

    fix(ui): do not destroy ThreeJS renderer before transition starts in App Intro

--- frontend/src/modules/client-site/components/LandingTransitionOverlay.tsx
+++ frontend/src/modules/client-site/components/LandingTransitionOverlay.tsx
@@ -440,7 +440,7 @@ export function LandingTransitionOverlay({ onComplete }: LandingTransitionOverla
         renderer.domElement.remove();
       }
     };
-  }, [introState]);
+  }, []);
 
   // Transitioning Phase
   useEffect(() => {
